b46b41229289377b04f03b2fe52453cd976636fb
Mike Spertus <msspertu@amazon.com>
2022-11-28 21:34:27 -0600

    Add static delay table

---
+++ pages/delays.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
+export default function delays() {
+    const [isSSR, setIsSSR] = useState(true);
+
+    useEffect(() => {
+        setIsSSR(false);
+    }, []);
+    return( !isSSR &&  <div><Head>
+        <title>Result</title>
+        <link type="text/css" rel="stylesheet" href="table.css" />
+      </Head>
+      <body>
+        <div style={{
+            marginLeft:"275px",
+            marginRight:"auto",
+            display:"inline-block",
+            boxShadow:"10px 10px 5px #888888",
+            border:"1px solid #000000",
+            background:"white"}}>
+          &nbsp;Flight Delays from ORD to AUS by weather&nbsp;</div>
+        <p style={{bottomMargin:"10px"}}>
+          <table className="CSS_Table_Example" style={{width:"60%", margin:"auto"}}>
+        <tr><td>Clear</td><td>Fog</td><td>Rain</td><td>Snow</td><td>Hail</td><td>Thunder</td><td>Tornado</td></tr>
+        <tr><td>1</td><td>2</td><td>3</td><td>4</td>
+          <td>5</td><td>6</td><td>7</td></tr>
+        
+        </table>
+        </p>
+    </body></div>)
+}
